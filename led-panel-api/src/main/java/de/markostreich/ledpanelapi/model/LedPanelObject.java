41e60a7bf1ca875236cf16aabddc270c9a6cb604
Marko Streich <ms@markostreich.de>
2024-03-31 00:02:46 +0100

    adjust model

--- led-panel-api/src/main/java/de/markostreich/ledpanelapi/model/LedPanelObject.java
+++ led-panel-api/src/main/java/de/markostreich/ledpanelapi/model/LedPanelObject.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -33,4 +34,7 @@ public class LedPanelObject {
 	private int rotationPointY;
 
 	private byte[] data;
+	
+	@ManyToOne
+	private LedPanelDevice device;
 }
